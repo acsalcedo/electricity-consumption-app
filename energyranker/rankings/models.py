d2ed541d9090fc99d7ce1ec12df749e36e0cb5e9
Andrea Salcedo <andrea.salcedo@runtastic.com>
2019-11-30 15:13:47 +0100

    Add initial Country and Ranking models

--- energyranker/rankings/models.py
+++ energyranker/rankings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+  iso_code = models.CharField(max_length=2, unique=True, db_index=True)
+  world_bank_id = models.CharField(max_length=3, unique=True, db_index=True)
+  name = models.CharField(max_length=200)
+
+class Ranking(models.Model):
+  country = models.ForeignKey(Country, on_delete=models.CASCADE)
+  year = models.IntegerField()
+  electricity_access = models.IntegerField()
+  energy_consumption = models.IntegerField()
